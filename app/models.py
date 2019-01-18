05412668ddde3683d8f7ec8aec35798cf79ca4dc
Jian <linjianviper@vip.qq.com>
2019-01-18 22:09:29 +0800

    flask web

--- app/models.py
+++ app/models.py
@@ -6,6 +6,13 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 # from flask import current_app
 
+class Permission:
+    FOLLOW = 0x01  # 关注其他用户
+    COMMENT = 0x02  # 在他人撰写的文章中发布评论
+    WRITE_ARTICLES = 0x04  # 写原创文章
+    MODERATE_COMMENTS = 0x08  # 查处他人发表的不当评论
+    ADMINISTER = 0x80  # 管理网站
+
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -14,6 +21,27 @@ class Role(db.Model):
     default = db.Column(db.Boolean, default=False, index=True)
     permissions = db.Column(db.Integer)
     users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __init__(self, **kwargs):
+        super(Role, self).__init__(**kwargs)
+        if self.permissions is None:
+            self.permissions = 0
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'User': (Permission.FOLLOW | Permission.COMMENT | Permission.WRITE_ARTICLES, True),
+            'Moderator': (
+                Permission.FOLLOW | Permission.COMMENT | Permission.WRITE_ARTICLES | Permission.MODERATE_COMMENTS,
+                False),
+            'Administrator': (0xff, False)
+        }
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.permissions = role[r][0]
+            role.default = role[r][1]
 
     def __repr__(self):
         return '<Role %r>' % self.name
@@ -45,11 +73,3 @@ class User(UserMixin, db.Model):
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
-
-class Permission:
-    FOLLOW = 0x01  # 关注其他用户
-    COMMENT = 0x02  # 在他人撰写的文章中发布评论
-    WRITE_ARTICLES = 0x04  # 写原创文章
-    MODERATE_COMMENTS = 0x08  # 查处他人发表的不当评论
-    ADMINISTER = 0x80  # 管理网站
